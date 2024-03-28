46bc3320b670f6454fbf03f19a85d34f81d5d925
dreaming-panda <64370186+dreaming-panda@users.noreply.github.com>
2024-03-27 23:24:45 -0400

    Create runy.sh

---
+++ tests/runy.sh
@@ -0,0 +1,29 @@
+CUDA_VISIBLE_DEVICES=5 python testbed_greedy.py --model  JackFram/llama-68m   --target meta-llama/Llama-2-7b-hf  --T 0.6 --P 1.0  --start 0 --end 200 --M 384 --growmap ../L40_growmaps/8-chain.pt  --Mode greedy --dataset openwebtext >> x2.log
+CUDA_VISIBLE_DEVICES=5 python testbed.py --model  JackFram/llama-68m   --target meta-llama/Llama-2-7b-hf  --T 0.6 --P 1.0  --start 0 --end 200 --M 384 --growmap ../L40_growmaps/8-chain.pt  --Mode greedy --dataset openwebtext >> x2.log
+
+CUDA_VISIBLE_DEVICES=5 python testbed_greedy.py --model  JackFram/llama-68m   --target meta-llama/Llama-2-7b-hf  --T 0.6 --P 1.0  --start 0 --end 200 --M 384 --growmap ../L40_growmaps/8-chain.pt  --Mode greedy --dataset cnn >> x2.log
+CUDA_VISIBLE_DEVICES=5 python testbed.py --model  JackFram/llama-68m   --target meta-llama/Llama-2-7b-hf  --T 0.6 --P 1.0  --start 0 --end 200 --M 384 --growmap ../L40_growmaps/8-chain.pt  --Mode greedy --dataset cnn >> x2.log
+
+#CUDA_VISIBLE_DEVICES=5 python testbed_greedy.py --model  JackFram/llama-68m   --target meta-llama/Llama-2-13b-hf  --T 0.6 --P 1.0  --start 0 --end 200 --M 384 --growmap ../L40_growmaps/8-chain.pt  --Mode greedy --dataset c4 >> x2.log
+#CUDA_VISIBLE_DEVICES=5 python testbed.py --model  JackFram/llama-68m   --target meta-llama/Llama-2-13b-hf  --T 0.6 --P 1.0  --start 0 --end 200 --M 384 --growmap ../L40_growmaps/8-chain.pt  --Mode greedy --dataset c4 >> x2.log
+
+CUDA_VISIBLE_DEVICES=5 python testbed_greedy.py --model  JackFram/llama-68m   --target meta-llama/Llama-2-13b-hf  --T 0.6 --P 1.0  --start 0 --end 200 --M 384 --growmap ../L40_growmaps/8-chain.pt  --Mode greedy --dataset openwebtext >> x2.log
+CUDA_VISIBLE_DEVICES=5 python testbed.py --model  JackFram/llama-68m   --target meta-llama/Llama-2-13b-hf  --T 0.6 --P 1.0  --start 0 --end 200 --M 384 --growmap ../L40_growmaps/8-chain.pt  --Mode greedy --dataset openwebtext >> x2.log
+
+CUDA_VISIBLE_DEVICES=5 python testbed_greedy.py --model  JackFram/llama-68m   --target meta-llama/Llama-2-13b-hf  --T 0.6 --P 1.0  --start 0 --end 200 --M 384 --growmap ../L40_growmaps/8-chain.pt  --Mode greedy --dataset cnn >> x2.log
+CUDA_VISIBLE_DEVICES=5 python testbed.py --model  JackFram/llama-68m   --target meta-llama/Llama-2-13b-hf  --T 0.6 --P 1.0  --start 0 --end 200 --M 384 --growmap ../L40_growmaps/8-chain.pt  --Mode greedy --dataset cnn >> x2.log
+
+#CUDA_VISIBLE_DEVICES=5 python testbed_greedy.py --model  JackFram/llama-160m   --target meta-llama/Llama-2-13b-hf  --T 0.6 --P 1.0  --start 0 --end 200 --M 384 --growmap ../L40_growmaps/8-chain.pt  --Mode greedy --dataset c4 >> x2.log
+#CUDA_VISIBLE_DEVICES=5 python testbed.py --model  JackFram/llama-160m    --target meta-llama/Llama-2-13b-hf  --T 0.6 --P 1.0  --start 0 --end 200 --M 384 --growmap ../L40_growmaps/8-chain.pt  --Mode greedy --dataset c4 >> x2.log
+
+CUDA_VISIBLE_DEVICES=5 python testbed_greedy.py --model  JackFram/llama-160m    --target meta-llama/Llama-2-13b-hf  --T 0.6 --P 1.0  --start 0 --end 200 --M 384 --growmap ../L40_growmaps/8-chain.pt  --Mode greedy --dataset openwebtext >> x2.log
+CUDA_VISIBLE_DEVICES=5 python testbed.py --model  JackFram/llama-160m    --target meta-llama/Llama-2-13b-hf  --T 0.6 --P 1.0  --start 0 --end 200 --M 384 --growmap ../L40_growmaps/8-chain.pt  --Mode greedy --dataset openwebtext >> x2.log
+
+CUDA_VISIBLE_DEVICES=5 python testbed_greedy.py --model  JackFram/llama-160m    --target meta-llama/Llama-2-13b-hf  --T 0.6 --P 1.0  --start 0 --end 200 --M 384 --growmap ../L40_growmaps/8-chain.pt  --Mode greedy --dataset cnn >> x2.log
+CUDA_VISIBLE_DEVICES=5 python testbed.py --model  JackFram/llama-160m    --target meta-llama/Llama-2-13b-hf  --T 0.6 --P 1.0  --start 0 --end 200 --M 384 --growmap ../L40_growmaps/8-chain.pt  --Mode greedy --dataset cnn >> x2.log
+
+CUDA_VISIBLE_DEVICES=5 python testbed_greedy.py --model  princeton-nlp/Sheared-LLaMA-1.3B    --target lmsys/vicuna-33b-v1.3  --T 0.6 --P 1.0  --start 0 --end 200 --M 384 --growmap ../L40_growmaps/8-chain.pt  --Mode greedy --dataset openwebtext >> x2.log
+CUDA_VISIBLE_DEVICES=5 python testbed.py --model  princeton-nlp/Sheared-LLaMA-1.3B    --target lmsys/vicuna-33b-v1.3  --T 0.6 --P 1.0  --start 0 --end 200 --M 384 --growmap ../L40_growmaps/8-chain.pt  --Mode greedy --dataset openwebtext >> x2.log
+
+CUDA_VISIBLE_DEVICES=5 python testbed_greedy.py --model  princeton-nlp/Sheared-LLaMA-1.3B    --target lmsys/vicuna-33b-v1.3  --T 0.6 --P 1.0  --start 0 --end 200 --M 384 --growmap ../L40_growmaps/8-chain.pt  --Mode greedy --dataset cnn >> x2.log
+CUDA_VISIBLE_DEVICES=5 python testbed.py --model  princeton-nlp/Sheared-LLaMA-1.3B    --target lmsys/vicuna-33b-v1.3  --T 0.6 --P 1.0  --start 0 --end 200 --M 384 --growmap ../L40_growmaps/8-chain.pt  --Mode greedy --dataset cnn >> x2.log
